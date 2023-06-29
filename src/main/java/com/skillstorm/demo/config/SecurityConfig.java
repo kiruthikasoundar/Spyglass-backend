6c6f6f79ac2b4761bfd0296538af9e80671f1076
kiruthikasoundar <36938852+kiruthikasoundar@users.noreply.github.com>
2023-06-29 18:26:40 -0400

    updated allowed origins

--- src/main/java/com/skillstorm/demo/config/SecurityConfig.java
+++ src/main/java/com/skillstorm/demo/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 		
 		http.cors().configurationSource(request -> {
 			CorsConfiguration corsConfig = new CorsConfiguration();
-			corsConfig.setAllowedOrigins(Arrays.asList(frontendUrl,"http://kiruthika-project3-spyglass.s3-website-us-east-1.amazonaws.com","http://Kiruthika-project2-backend-elastic-env.eba-eethncyr.us-east-1.elasticbeanstalk.com"));
+			corsConfig.setAllowedOrigins(Arrays.asList(frontendUrl,"http://kiruthika-project3-spyglass.s3-website-us-east-1.amazonaws.com","http://Kiruthika-project2-backend-elastic-env.eba-eethncyr.us-east-1.elasticbeanstalk.com", "https://accounts.google.com"));
 //			corsConfig.addAllowedOrigin(frontendUrl);
 //			corsConfig.addAllowedOrigin("http://kiruthika-project3-spyglass.s3-website-us-east-1.amazonaws.com*");
 //			corsConfig.addAllowedOrigin("http://Kiruthika-project2-backend-elastic-env.eba-eethncyr.us-east-1.elasticbeanstalk.com*");
@@ -48,7 +48,7 @@ public class SecurityConfig {
 			corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","Origin", "X-Auth-Token"));
 			corsConfig.setAllowCredentials(true);
 			corsConfig.setMaxAge(3600L);
-			corsConfig.addAllowedOrigin("https://accounts.google.com");
+//			corsConfig.addAllowedOrigin("https://accounts.google.com");
 			corsConfig.addAllowedHeader("Authorization");
 			corsConfig.addExposedHeader("Authorization"); 
 			corsConfig.addAllowedHeader("*");
